c9c150c730790aa8215677f7db0d4887b970dfa6
txidol <love_xiang21@foxmail.com>
2014-01-17 14:46:03 +0800

    demo

---
+++ src/main/java/com/txidol/controller/ShowMessageController.java
@@ -0,0 +1,16 @@
+package com.txidol.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>Copyright:Copyright(c)2013</p>
+ * <p>Company:Mopon</p>
+ * <p>Description: </p>
+ * @date 2014年1月17日
+ * @author 谭翔
+ * @version 1.0
+ */
+@Controller
+public class ShowMessageController {
+
+}
---
+++ src/test/java/com/txidol/controller/ShowMessageControllerTest.java
@@ -0,0 +1,15 @@
+package com.txidol.controller;
+
+
+/**
+ * <p>Copyright:Copyright(c)2013</p>
+ * <p>Company:Mopon</p>
+ * <p>Description: </p>
+ * @date 2014年1月17日
+ * @author 谭翔
+ * @version 1.0
+ */
+
+public class ShowMessageControllerTest {
+
+}
